5ab863bc7b9fbf2cc46c11d0b4a05eb33625b75e
France Raphael RIvera <francerivera1231@gmail.com>
2025-04-03 22:59:36 +0800

    Delete shop directory

--- shop/connections.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connections = mysqli_connect("localhost", "root", "", "shopping_db");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-?>
--- shop/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Start the session
-session_destroy(); // Destroy all session data
-header("Location: login.php"); // Redirect to the login page
-exit(); // Ensure no further code is executed
-?>
